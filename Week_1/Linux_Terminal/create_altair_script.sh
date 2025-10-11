46250cda07733aa97c74702662898db895d22410
Azmain Adeeb <azmainadeeb@192.168.1.3>
2025-10-11 03:02:52 -0700

    added .sh file

---
+++ Week_1/Linux_Terminal/create_altair_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir -p ~/altair_software_recruitment/executables/
+cat > ~/altair_software_recruitment/executables/hello_altair.py << 'EOF'
+#!/usr/bin/env python3
+print("Hello, Altair!")
+EOF
+sudo chown root:root ~/altair_software_recruitment/executables/hello_altair.py
+sudo chmod 700 ~/altair_software_recruitment/executables/hello_altair.py
